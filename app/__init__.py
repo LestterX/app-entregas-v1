ad07f3c6b628863bb28353485ffcdebcb905600c
LestterX <lgs.alves@hotmail.com>
2023-12-12 16:41:59 -0300

    config: change flask import

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+import flask 
 from .static.database.database_controller import *
 
 create_database()
 
 def start_app():
-    app = Flask(__name__)
+    app = flask.Flask(__name__)
     from .views import views
     app.register_blueprint(views, url_prefix='/')
     return app
