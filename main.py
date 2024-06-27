9d38b64f377d0c1b8815c1d6c1fb0fc79ccfd562
LestterX <lgs.alves@hotmail.com>
2024-06-27 15:11:22 -0300

    solved: access to docker container from outside

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ app = start_app()
 
 if __name__ == '__main__':
 #                ''                    '5058'
-    app.run(host='', port='5050', debug=True)
+    app.run(host='0.0.0.0', port='5050', debug=True) # 0.0.0.0 permite acesso fora do container docker
     
